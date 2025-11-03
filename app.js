101de44e0a6fd47c69d14e3fe0a180c41ba3ff8b
Mrkhun99 <Khunkimlim10@gmail.com>
2025-11-03 16:33:27 +0700

    Update greeting message

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello, GitHub Actions!");
+app.get('/', (req, res) => {
+  res.send('Hello, CI/CD!');
 });
+
 module.exports = app;
